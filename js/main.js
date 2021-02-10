b5cff5f2af0c8d2497fd2452a505153f08c634d1
Yonatan Mizrahi <yoni9696@walla.com>
2021-02-10 14:54:16 +0200

    created main page

--- js/main.js
+++ js/main.js
@@ -1,1 +1,42 @@
 'use strict';
+
+
+
+
+var gElCanvas;
+var gCtx;
+var gImg;
+
+function initCanvas() {
+    gElCanvas = document.getElementById('my-canvas');
+    gCtx = gElCanvas.getContext('2d');
+    resizeCanvas();
+}
+
+
+function resizeCanvas() {
+    var elContainer = document.querySelector('.canvas-container');
+    gElCanvas.width = elContainer.offsetWidth;
+    gElCanvas.height = elContainer.offsetWidth;
+    // gElCanvas.height = elContainer.offsetHeight;
+}
+
+
+function onLoadImage(id) {
+    setMeme(id);
+    initCanvas();
+    let image = getImageById(id);
+    var img = new Image();
+    img.onload = () => (renderImg(img));
+    img.src = image.url;
+}
+
+
+function renderImg(img) {
+    gCtx.drawImage(img, 0, 0, gElCanvas.width, gElCanvas.height);
+}
+
+
+function getImageById(id) {
+    return getImage(id);
+}
--- js/meme-service.js
+++ js/meme-service.js
@@ -1,5 +1,14 @@
 'use strict';
 
+var gMeme;
+
+
+// var gMeme = {
+//     selectedImgId: 5,
+//     selectedLineIdx: 0,
+//     lines: [{ txt: 'I never eat Falafel', size: 20, align: 'left', color: 'red' }]
+// }
+
 var gKeywords = {
     'happy': 12,
     'funny puk': 1
@@ -7,13 +16,25 @@ var gKeywords = {
 
 
 var gImgs = [
-    { id: 1, url: 'img/popo.jpg', keywords: ['happy'] },
-    { id: 1, url: 'img/popo.jpg', keywords: ['happy'] }
+    { id: 1, url: 'meme-imgs/1.jpg', keywords: ['happy'] },
+    { id: 2, url: 'meme-imgs/2.jpg', keywords: ['happy'] }
 ];
 
 
-var gMeme = {
-    selectedImgId: 5,
-    selectedLineIdx: 0,
-    lines: [{ txt: 'I never eat Falafel', size: 20, align: 'left', color: 'red' }]
+
+function getImage(id) {
+    return gImgs.find(img => {
+        return img.id === id;
+    });
+}
+
+function setMeme(id) {
+    let meme = {
+        selectedImgId: id,
+        selectedLineIdx: 0,
+        lines: [{ txt: 'I never eat Falafel', size: 20, align: 'left', color: 'red' }]
+    };
+
+    gMeme = meme;
+    console.log(gMeme);
 }
