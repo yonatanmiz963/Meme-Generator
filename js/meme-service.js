42f7fea4c13f38144ee8e9882bed232b0e4c135d
Yonatan Mizrahi <yoni9696@walla.com>
2021-02-10 19:57:05 +0200

    first delivery commit

--- js/meme-service.js
+++ js/meme-service.js
@@ -18,7 +18,8 @@ var gKeywords = {
 
 var gImgs = [
     { id: 1, url: 'meme-imgs/1.jpg', keywords: ['happy'] },
-    { id: 2, url: 'meme-imgs/2.jpg', keywords: ['happy'] }
+    { id: 2, url: 'meme-imgs/2.jpg', keywords: ['happy'] },
+    { id: 3, url: 'meme-imgs/3.jpg', keywords: ['happy'] }
 ];
 
 
